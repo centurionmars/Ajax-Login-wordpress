49cf2710344f85b959b9f6d4bf4ada756ed4a651
centurionmars <resident4f@gmail.com>
2022-05-06 15:49:44 +0430

    init

---
+++ ajax_login.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wordpress Auth
+Plugin URI: https://www.7learn.com
+Description: wordpress plugin to manage auth
+Author: mars
+Author URI: https://cyclestart.ir
+Text Domain: wordpress-auth
+Domain Path: /languages/
+Version: 1.0.0
+*/
+
