7944b974642813d6adc09024a924016fdc7098f0
centurionmars <resident4f@gmail.com>
2022-05-07 15:11:50 +0430

    style form's

--- tpl/front/login.php
+++ tpl/front/login.php
@@ -1,1 +1,18 @@
-<h1>ورود</h1>
+<div class="auth-wrapper">
+    <div class="login-wrapper">
+        <form action="">
+            <div class="form-row">
+                <label for="userEmail" class="textLabel" ></label>
+                <input type="text" placeholder="ایمیل" class="textInput" name="userEmail" id="userEmail">
+            </div>
+            <div class="form-row">
+                <label for="userPassword" class="textLabel"></label>
+                <input type="password" placeholder="رمز عبور" class="textInput" name="userPassword" id="userPassword">
+            </div>
+            <div class="form-row">
+                <button name="submitLogin"
+                class="loginButton">ورود</button>
+            </div>
+        </form>
+    </div>
+</div>
--- tpl/front/register.php
+++ tpl/front/register.php
@@ -1,2 +1,21 @@
-
-<h1>ثبت نام</h1>
+<div class="auth-wrapper">
+    <div class="register-wrapper">
+        <form action="">
+            <div class="form-row">
+                <label for="fName"></label>
+                <input type="text" placeholder="نام" class="textInput" name="fName" id="fName">
+            </div>
+            <div class="form-row">
+                <label for="lName" class="textLabel"></label>
+                <input type="text" placeholder="نام خانوادگی" class="textInput" name="lName" id="lName">
+            </div>
+            <div class="form-row">
+                <label for="userPassword" class="textLabel"></label>
+                <input type="password" placeholder="کلمه عبور" class="textInput" name="userPassword" id="userPassword">
+            </div>
+            <div class="form-row">
+                <button name="submitLogin" class="loginButton">ورود</button>
+            </div>
+        </form>
+    </div>
+</div>
